1e6efcafd28cfe67f5e62fe21b079097ebf03fbb
Rajin-afk <68631273+Rajin-afk@users.noreply.github.com>
2024-01-18 19:55:28 +0600

    Add files via upload

---
+++ ecom/index.php
@@ -0,0 +1,77 @@
+<?php
+
+//if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
+if(session_id() == '' || !isset($_SESSION)){session_start();}
+
+?>
+
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Shop NearBy</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+   
+  </head>
+  <body>
+
+    <nav class="top-bar" data-topbar role="navigation">
+      <ul class="title-area">
+        <li class="name">
+          <h1><a href="index.php">Shop NearBy</a></h1>
+        </li>
+        <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+      </ul>
+
+      <section class="top-bar-section">
+      <!-- Right Nav Section -->
+        <ul class="right">
+          <li><a href="about.php">About</a></li>
+          <li><a href="products.php">Products</a></li>
+          <li><a href="cart.php">View Cart</a></li>
+          <li><a href="orders.php">My Orders</a></li>
+          <li><a href="contact.php">Contact</a></li>
+          <?php
+
+          if(isset($_SESSION['username'])){
+            echo '<li><a href="account.php">My Account</a></li>';
+            echo '<li><a href="logout.php">Log Out</a></li>';
+          }
+          else{
+            echo '<li><a href="login.php">Log In</a></li>';
+            echo '<li><a href="register.php">Register</a></li>';
+          }
+          ?>
+        </ul>
+      </section>
+    </nav>
+
+    <img data-interchange="[images/bolt-retina.jpeg, (retina)], [images/bolt-landscape.jpeg, (large)], [images/bolt-mobile.jpeg, (mobile)], [images/bolt-landscape.jpeg, (medium)]">
+    <noscript><img src="images/bolt-landscape.jpeg"></noscript>
+
+
+    
+
+
+    <div class="row" style="margin-top:10px;">
+      <div class="small-12">
+
+        <footer style="margin-top:10px;">
+           
+        </footer>
+
+      </div>
+    </div>
+
+
+
+
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
